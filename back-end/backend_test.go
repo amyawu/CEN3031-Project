c23a880f050d024d491f01f5c53eedf4994cd87e
ManuelCortes23 <73561114+ManuelCortes23@users.noreply.github.com>
2023-02-17 16:53:27 -0500

    added unit testing

---
+++ back-end/backend_test.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"testing"
+	config "web-service-gin/configs"
+)
+
+func SetUpRouter() *gin.Engine {
+	router := gin.Default()
+	return router
+}
+
+func TestRouter(t *testing.T) {
+	// Create a new router
+	r := SetUpRouter()
+
+	// Test if the router is not nil
+	if r == nil {
+		t.Errorf("Router is nil")
+	}
+}
+
+func TestCreateUser(t *testing.T) {
+	// Create a new Gin router
+	r := SetUpRouter()
+
+	// Define the routes
+	r.POST("/users", createUser)
+
+	// Create a new user to add to the database
+	newUser := config.User{
+		Name:  "John Smith",
+		Email: "john.smith@example.com",
+	}
+
+	// Convert the user struct to JSON
+	jsonData, err := json.Marshal(newUser)
+	if err != nil {
+		t.Errorf("error marshaling JSON: %v", err)
+	}
+
+	// Create a new HTTP request
+	req, err := http.NewRequest("POST", "/users", bytes.NewBuffer(jsonData))
+	if err != nil {
+		t.Errorf("error creating HTTP request: %v", err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	//// Create a new HTTP response recorder
+	//w := httptest.NewRecorder()
+	//
+	//// Perform the request
+	//r.ServeHTTP(w, req)
+	//
+	//// Check the response status code
+	//if w.Code != http.StatusCreated {
+	//	t.Errorf("expected status code %d but got %d", http.StatusCreated, w.Code)
+	//}
+
+	//// Check that the response body contains the created user
+	//var createdUser config.User
+	//err = json.Unmarshal(w.Body.Bytes(), &createdUser)
+	//if err != nil {
+	//	t.Errorf("error unmarshaling JSON: %v", err)
+	//}
+	//if createdUser.Name != newUser.Name {
+	//	t.Errorf("expected user name %s but got %s", newUser.Name, createdUser.Name)
+	//}
+	//if createdUser.Email != newUser.Email {
+	//	t.Errorf("expected user email %s but got %s", newUser.Email, createdUser.Email)
+	//}
+}
--- back-end/go.mod
+++ back-end/go.mod
@@ -20,14 +20,11 @@ require (
 	cloud.google.com/go/iam v0.8.0 // indirect
 	cloud.google.com/go/longrunning v0.3.0 // indirect
 	cloud.google.com/go/storage v1.27.0 // indirect
-	firebase.google.com/go/v4 v4.10.0 // indirect
-	github.com/MicahParks/keyfunc v1.5.1 // indirect
 	github.com/creasty/defaults v1.5.1 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/goccy/go-json v0.10.0 // indirect
-	github.com/golang-jwt/jwt/v4 v4.4.2 // indirect
 	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
@@ -54,7 +51,6 @@ require (
 	golang.org/x/time v0.1.0 // indirect
 	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
-	google.golang.org/appengine/v2 v2.0.2 // indirect
 	google.golang.org/genproto v0.0.0-20230209215440-0dfe4f8abfcc // indirect
 	google.golang.org/grpc v1.53.0 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
