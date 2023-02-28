4089e3cdefbccebbbeb0ebfd9bc7fd775a14b5ab
Jose Pancorbo <JosePancorbo1@outlook.com>
2023-02-28 18:59:47 -0500

    Fixed cypress, tests login page

--- front-end/testing/src/app/app.module.ts
+++ front-end/testing/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 //import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
--- front-end/testing/src/app/login/login.component.cy.ts
+++ front-end/testing/src/app/login/login.component.cy.ts
@@ -1,12 +1,10 @@
+import { HttpClientModule } from "@angular/common/http"
 import { LoginComponent } from "./login.component"
 
 describe('LoginComponent', () => {
-    it('can access the URL', () => {
-        // clicking the anchor causes the browser to follow the link
-        cy.window()
-    })
-    it('can submit an empty response', () => {
-        // clicking the anchor causes the browser to follow the link
-        cy.get('input').submit() // Submit a form
-    })
+    it('mounts', () => {
+        cy.mount(LoginComponent, {
+          imports: [HttpClientModule]
+        })
+      })
 })
