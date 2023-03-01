c1bd7701cf867a1c124d7e2497e877aff7252f76
amyawu <68081958+amyawu@users.noreply.github.com>
2023-03-01 13:28:35 -0500

    Completed unit testing with the components for uploand login and register

--- front-end/testing/src/app/login/login.component.cy.ts
+++ front-end/testing/src/app/login/login.component.cy.ts
@@ -23,7 +23,7 @@ describe('LoginComponent', () => {
         cy.get('[data-cy=login_form]').submit()
       })
 
-      it.only('login with response', () => {
+      it.only('fill out login with response', () => {
         cy.mount(LoginComponent, {
             imports: [BrowserModule,
               HttpClientModule,
@@ -31,5 +31,38 @@ describe('LoginComponent', () => {
           ]
           })
         cy.get('[data-cy="login_email_data"]').type('email@gmail.com')
-      })
+        cy.get('[data-cy="login_password_data"]').type('emailemail')
+    })
+
+    it.only('login with filled out response', () => {
+        cy.mount(LoginComponent, {
+            imports: [BrowserModule,
+              HttpClientModule,
+              FormsModule
+          ]
+          })
+          cy.get('[data-cy="login_email_data"]').type('email@gmail.com')
+        cy.get('[data-cy="login_password_data"]').type('emailemail')
+        cy.get('[data-cy=login_form]').submit()
+    })
+
+    it.only('check if we can switch to register page from login', () => {
+        cy.mount(LoginComponent, {
+            imports: [BrowserModule,
+              HttpClientModule,
+              FormsModule
+          ]
+          })
+          cy.get('[data-cy=register_button_login]').should('have.attr', 'href', '/register')
+    })
+    it.only('switches to register page from login', () => {
+        cy.mount(LoginComponent, {
+            imports: [BrowserModule,
+              HttpClientModule,
+              FormsModule
+          ]
+          })
+          cy.get('[data-cy=register_button_login]').should('have.attr', 'href', '/register')
+          cy.get('[data-cy=register_button_login]').click();
+    })
 })
--- front-end/testing/src/app/register/register.component.cy.ts
+++ front-end/testing/src/app/register/register.component.cy.ts
@@ -22,4 +22,47 @@ describe('RegisterComponent', () => {
           })
         cy.get('[data-cy=register_form]').submit()
       })
+
+      it.only('fill out login with response', () => {
+        cy.mount(RegisterComponent, {
+            imports: [BrowserModule,
+              HttpClientModule,
+              FormsModule
+          ]
+          })
+        cy.get('[data-cy="register_email_data"]').type('email@gmail.com')
+        cy.get('[data-cy="register_password_data"]').type('emailemail')
+    })
+
+    it.only('fill out login with response', () => {
+        cy.mount(RegisterComponent, {
+            imports: [BrowserModule,
+              HttpClientModule,
+              FormsModule
+          ]
+          })
+        cy.get('[data-cy="register_email_data"]').type('email@gmail.com')
+        cy.get('[data-cy="register_password_data"]').type('emailemail')
+        cy.get('[data-cy=register_form]').submit()
+    })
+
+    it.only('check if we can switch to register page from login', () => {
+        cy.mount(RegisterComponent, {
+            imports: [BrowserModule,
+              HttpClientModule,
+              FormsModule
+          ]
+          })
+          cy.get('[data-cy=login_button_register]').should('have.attr', 'href', '/login')
+    })
+    it.only('switches to register page from login', () => {
+        cy.mount(RegisterComponent, {
+            imports: [BrowserModule,
+              HttpClientModule,
+              FormsModule
+          ]
+          })
+          cy.get('[data-cy=login_button_register]').should('have.attr', 'href', '/login')
+          cy.get('[data-cy=login_button_register]').click();
+    })
 })
