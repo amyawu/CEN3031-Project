bda82ee0f568eca60e73059e7ec2b46551a138de
Jose Pancorbo <JosePancorbo1@outlook.com>
2023-04-19 19:53:57 -0400

    updated authorization

--- back-end/back_end_v3_GORM.go
+++ back-end/back_end_v3_GORM.go
@@ -99,7 +99,7 @@ func main() {
 	// Enable CORS
 	config := cors.DefaultConfig()
 	config.AllowOrigins = []string{"http://localhost:4200", "http://localhost:8080", "http://localhost:8081"}
-	config.AllowHeaders = []string{"authorization"}
+	config.AllowHeaders = []string{"Authorization"}
 	r.Use(cors.New(config))
 
 	// Define the routes
