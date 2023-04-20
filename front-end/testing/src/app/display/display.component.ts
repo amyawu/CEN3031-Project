77de44f8ecf900d91191ce7240fc745dc102bca8
Jose Pancorbo <JosePancorbo1@outlook.com>
2023-04-19 21:16:58 -0400

    display image page is live!!

--- front-end/testing/src/app/display/display.component.ts
+++ front-end/testing/src/app/display/display.component.ts
@@ -9,28 +9,31 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./display.component.css']
 })
 export class DisplayComponent {
-  displayUserData:any = {};
-  imageUrls:any = []
+  displayUserData: any = {};
+  imageUrls: any = [];
 
   constructor(private _auth: AuthService, private http: HttpClient) {
     this.displayUser();
+  }
+
+  get authService() {
+    return this._auth;
   }
 
   displayUser() {
     let token = localStorage.getItem('token');
     let tstring = '{"token": ' + '"' + token + '"}';
     let jsonobj = JSON.parse(tstring);
-    
-    this.http.get<any>('http://localhost:8000/users/all_images', jsonobj).subscribe(
+
+    this.http.post<any>('http://localhost:8000/users/all_images', jsonobj).subscribe(
       res => {
-        console.log(res)
+        console.log(res);
         this.displayUserData = res;
+        this.imageUrls = res.urls;
       },
       err => {
         console.log(err);
       }
     );
   }
-
-
 }
